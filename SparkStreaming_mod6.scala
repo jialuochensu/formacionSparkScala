f7cf7c2bd76c948d7abd7341dea0b8436ac15322
Jialuo Chen Su <jialuochensu@gmail.com>
2023-03-01 13:16:58 +0100

    Modulo 6

---
+++ SparkStreaming_mod6.scala
@@ -0,0 +1,40 @@
+//Spark Streaming I - Modulo 6.1
+//Iniciarnos en el uso de Spark Streaming
+
+//imports
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.Seconds
+
+//Crear un ssc con duracion 5 sec
+val ssc = new StreamingContext(sc, Seconds(5))
+//Crea un DStream para leer texto del puerto que pusiste en el comando “nc”, especificando el hostname de nuestra máquina
+val mystream = ssc.socketTextStream("quickstart.cloudera",4444)
+//mapReduce, para contar las palabrasque aparece en cada stream
+val words = mystream.flatMap(line => line.split("\\W"))
+val wordCounts = words.map(x => (x, 1)).reduceByKey((x,y) => x+y)
+wordCounts.print()
+//Arrancar el ssc y llamar awaitTermination para esperar a que la tarea termine
+ssc.start()
+ssc.awaitTermination()
+
+//Spark Streaming II - Modulo 6.2
+//imports
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.Seconds
+
+//Crear un ssc con duracion 5 sec
+var ssc=new StreamingContext(sc, Seconds(5))
+//Crea un DStream, especificando el hostname(quickstart.cloudera) de nuestra máquina
+var dstream=ssc.socketTextStream("quickstart.cloudera",4444)
+//Filtra las líneas del Stream que contengan la cadena de caracteres “KBDOC”
+val lineas =dstream.filter(x=>x.contains("KBDOC"))
+
+//Para cada RDD, imprime el número de líneas que contienen la cadena de caracteres indicada. Para ello, puedes usar la función “foreachRDD()”.
+var numero=foreachRDD(líneas,1)
+//Guarda el resultado del filtrado en un fichero de texto en sistema de archivos local
+Numero=ssc.saveAsTextFiles(“/home/Cloudera/…”)
+
+ssc.start()
+b. ssc.awaitTermination()
